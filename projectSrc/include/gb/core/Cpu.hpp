576a1ac01a9b0ecc2a4d84557dd20d9ac6990d9c
Michael barbarin <michael.barbarin@gmail.com>
2016-11-03 10:38:09 +0100

    first fix compile and add thomas's dev, need some fix again before push master

--- projectSrc/include/gb/core/Cpu.hpp
+++ projectSrc/include/gb/core/Cpu.hpp
@@ -1,63 +1,91 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   cpu.hpp                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: barbare <barbare@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/10/27 14:09:39 by barbare           #+#    #+#             */
-/*   Updated: 2016/10/27 19:28:57 by barbare          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef _CPU_HPP
 # define _CPU_HPP
 
-# include "Timer.hpp"
+#include "Timer.hpp"
+#include "Memory.hpp"
 
 #define REGISTER_TIMA	0xFF05
 #define REGISTER_TMA	0xFF06
 #define REGISTER_TAC	0xFF07
 #define REGISTER_IF		0xFF0F //TODO timer overflow??? je peut le bypasser par le timer de base
 
-#define 8_BITS = 8
-#define 16_BITS = 16
+#define NB_OPCODE 255
+#define NOP_OPCODE 0
 
-struct s_opcode
+typedef struct	s_opcode
 {
-	bool execute;
-	struct opcode
-	{
-		unsigned int opcode;
-		unsigned int cycleOpcode;
-		unsigned int lenghtData;
-		void *functionOpcode;
-		struct data
-		{
-		}
-	}
-}
+	uint8_t	opcode;
+	int8_t	mask;
+	uint8_t	cycleOpcodeFlag;
+	uint8_t cycleOpcodeNoFlag;
+	uint8_t lengthData;
+	void	(*functionOpcode)();
+}				t_opcode;
 
-class Cpu_z80 : Cpu, RegisterZ80
+class Cpu_z80
 {
+	/*
+	** ################################################################
+	** CREATE Singleton
+	** ################################################################
+	*/
 	public:
-		Cpu_z80(void) : Cpu(8_BITS, _arrayFrequency, RegisterZ80::register);
-		~Cpu_z80(void);
+		Cpu_z80 &Instance();
 
 	private:
-		std::array<int, 4> _arrayFrequency{ 4096, 262144, 65536, 16384 };
-};
+		Cpu_z80(void);
+		~Cpu_z80(void);
+		static Cpu_z80	_instance;
 
-class Cpu : Register
-{
-	protected:
-		Cpu(unsigned int addressLength, std::array<int, 4> arrFrequency, Register register);
-		~Cpu(void);
-		std::array<unsigned int, 4> _arrayFrequency;
-		unsigned int ptrAddr;
-		unsigned int addrLenght;
-		Register register;
-		s_reg regInProgress;
-}
+	/*
+	** ################################################################
+	** METHOD opcode
+	** ################################################################
+	*/
+
+	public:
+		unsigned int nbCycleNextOpCode(void);
+		unsigned int executeNextOpcode(void);
+
+	private:
+		uint8_t _getNextOpcode();
+		t_opcode _getOpcode(unsigned int opcode);
+		unsigned int _getCycleOpcode(void);
+		void *_getFunctionOpcode(void);
+		unsigned int _getDataOpcode(void); //TODO: can create type 8b or 16b data, moment use uint
+		unsigned int _getLenghtDataOpcode(void);
+
+	/*
+	** ################################################################
+	** METHOD opcode
+	** ################################################################
+	*/
+
+	public:
+		std::array<unsigned int, 4> getArrayFrequency();
+		void interrupt(void);
+
+	private:
+		void _resetPtrAddr(void);
+		void _nextPtr(void);
+
+	/*
+	** ################################################################
+	** ATTRIBUTE
+	** ################################################################
+	*/
+	private:
+		unsigned int				_ptrAddr;
+		Memory						&_memory;
+		std::array<unsigned int, 4>	_arrayFrequency;
+		t_opcode					_opcodeMap[NB_OPCODE];
+		t_opcode					opcodeInProgress;
+		uint16_t					_pc; //Program counter
+		uint16_t					sp;
+
+	public:
+		unsigned int _addrLenght;
+
+};
 
 #endif
--- projectSrc/src/gb/core/Cpu.cpp
+++ projectSrc/src/gb/core/Cpu.cpp
@@ -6,41 +6,158 @@
 /*   By: barbare <barbare@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/27 14:10:07 by barbare           #+#    #+#             */
-/*   Updated: 2016/10/27 19:04:53 by barbare          ###   ########.fr       */
+/*   Updated: 2016/11/03 10:34:49 by barbare          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cpu.hpp"
 
+/*
+** ################################################################
+** CREATE Singleton
+** ################################################################
+*/
+
 Cpu::Cpu(unsigned int cpuAddrLenght,
 			std::array<unsigned int, 4> arrFrequency,
 			Register register) :	addrLenght(cpuAddrLenght),
 									ptrAddr(0x00),
+									_opcodeMap {
+										{0x00, 0x00, 4 , 4 , 1, NULL},
+										{0x01, 0x00, 12, 12, 3, NULL},
+										{0x02, 0x00, 8 , 8 , 1, NULL},
+										{0x03, 0x00, 8 , 8 , 1, NULL},
+										{0x04, 0x00, 4 , 4 , 1, NULL},
+										{0x05, 0x00, 4 , 4 , 1, NULL},
+										{0x06, 0x00, 8 , 8 , 2, NULL},
+										{0x07, 0x00, 4 , 4 , 1, NULL},
+										{0x08, 0x00, 20, 20, 3, NULL},
+										{0x09, 0x00, 8 , 8 , 1, NULL},
+										{0x0a, 0x00, 8 , 8 , 1, NULL},
+										{0x0b, 0x00, 8 , 8 , 1, NULL},
+										{0x0c, 0x00, 4 , 4 , 1, NULL},
+										{0x0d, 0x00, 4 , 4 , 1, NULL},
+										{0x0e, 0x00, 8 , 8 , 2, NULL},
+										{0x0f, 0x00, 4 , 4 , 1, NULL},
+										{0x10, 0x00, 4 , 4 , 2, NULL},
+										{0x11, 0x00, 12, 12, 3, NULL},
+										{0x12, 0x00, 8 , 8 , 1, NULL},
+										{0x13, 0x00, 8 , 8 , 1, NULL},
+										{0x14, 0x00, 4 , 4 , 1, NULL},
+										{0x15, 0x00, 4 , 4 , 1, NULL},
+										{0x16, 0x00, 8 , 8 , 2, NULL},
+										{0x17, 0x00, 4 , 4 , 1, NULL},
+										{0x18, 0x00, 12, 12, 2, NULL},
+										{0x19, 0x00, 8 , 8 , 1, NULL},
+										{0x1a, 0x00, 8 , 8 , 1, NULL},
+										{0x1b, 0x00, 8 , 8 , 1, NULL},
+										{0x1c, 0x00, 4 , 4 , 1, NULL},
+										{0x1d, 0x00, 4 , 4 , 1, NULL},
+										{0x1e, 0x00, 8 , 8 , 2, NULL},
+										{0x1f, 0x00, 4 , 4 , 1, NULL},
+										{0x20, 0xc0, 12, 8 , 2, NULL},
+										{0x21, 0x00, 12, 12, 3, NULL},
+										{0x22, 0x00, 8 , 8 , 1, NULL},
+										{0x23, 0x00, 8 , 8 , 1, NULL},
+										{0x24, 0x00, 4 , 4 , 1, NULL},
+										{0x25, 0x00, 4 , 4 , 1, NULL},
+										{0x26, 0x00, 12, 12, 2, NULL},
+										{0x27, 0x00, 4 , 4 , 1, NULL},
+										{0x28, 0x80, 12, 8 , 2, NULL},
+										{0x29, 0x00, 8 , 8 , 1, NULL},
+										{0x2a, 0x00, 8 , 8 , 1, NULL},
+										{0x2b, 0x00, 8 , 8 , 1, NULL},
+										{0x2c, 0x00, 4 , 4 , 1, NULL},
+										{0x2d, 0x00, 4 , 4 , 1, NULL},
+										{0x2e, 0x00, 8 , 8 , 2, NULL},
+										{0x2f, 0x00, 4 , 4 , 3, NULL},
+										{0x30, 0x70, 12, 8 , 2, NULL},
+										{0x31, 0x00, 12, 12, 3, NULL},
+										{0x32, 0x00, 12, 12, 3, NULL},
+										{0x33, 0x00, 8 , 8 , 1, NULL},
+										{0x34, 0x00, 12, 12, 1, NULL},
+										{0x35, 0x00, 12, 12, 1, NULL},
+										{0x36, 0x00, 12, 12, 2, NULL},
+										{0x37, 0x00, 4 , 4 , 1, NULL},
+										{0x38, 0x10, 12, 8 , 2, NULL},
+										{0x39, 0x00, 8 , 8 , 1, NULL},
+										{0x3a, 0x00, 8 , 8 , 1, NULL},
+										{0x3b, 0x00, 8 , 8 , 1, NULL},
+										{0x3c, 0x00, 4 , 4 , 1, NULL},
+										{0x3d, 0x00, 4 , 4 , 1, NULL},
+										{0x3e, 0x00, 8 , 8 , 2, NULL},
+										{0x3f, 0x00, 4 , 4 , 1, NULL},
+										{0x40, 0x00, 4 , 4 , 1, NULL},
+										{0x41, 0x00, 4 , 4 , 1, NULL},
+										{0x42, 0x00, 4 , 4 , 1, NULL},
+										{0x43, 0x00, 4 , 4 , 1, NULL},
+										{0x44, 0x00, 4 , 4 , 1, NULL},
+										{0x45, 0x00, 4 , 4 , 1, NULL},
+										{0x46, 0x00, 8 , 8 , 1, NULL},
+										{0x47, 0x00, 4 , 4 , 1, NULL},
+										{0x48, 0x00, 4 , 4 , 1, NULL},
+										{0x49, 0x00, 4 , 4 , 1, NULL},
+										{0x4a, 0x00, 4 , 4 , 1, NULL},
+										{0x4b, 0x00, 4 , 4 , 1, NULL},
+										{0x4c, 0x00, 4 , 4 , 1, NULL},
+										{0x4d, 0x00, 4 , 4 , 1, NULL},
+										{0x4e, 0x00, 8 , 8 , 1, NULL},
+										{0x4f, 0x00, 4 , 4 , 1, NULL},
+										{0x50, 0x00, 4 , 4 , 1, NULL},
+										{0x51, 0x00, 4 , 4 , 1, NULL},
+										{0x52, 0x00, 4 , 4 , 1, NULL},
+										{0x53, 0x00, 4 , 4 , 1, NULL},
+										{0x54, 0x00, 4 , 4 , 1, NULL},
+										{0x55, 0x00, 4 , 4 , 1, NULL},
+										{0x56, 0x00, 8 , 8 , 1, NULL},
+										{0x57, 0x00, 4 , 4 , 1, NULL},
+										{0x58, 0x00, 4 , 4 , 1, NULL},
+										{0x59, 0x00, 4 , 4 , 1, NULL},
+										{0x5a, 0x00, 4 , 4 , 1, NULL},
+										{0x5b, 0x00, 4 , 4 , 1, NULL},
+										{0x5c, 0x00, 4 , 4 , 1, NULL},
+										{0x5d, 0x00, 4 , 4 , 1, NULL},
+										{0x5e, 0x00, 8 , 8 , 1, NULL},
+										{0x5f, 0x00, 4 , 4 , 1, NULL},
+										{0x60, 0x00, 4 , 4 , 1, NULL},
+									}
+}
 									register(register)
 {
 	regInProgress.execute = true;
 }
 
-s_opcode Cpu::getOpcode(unsigned int opcode)
+Cpu_z80 &Cpu_z80::Instance(void)
+{
+	return this->_instance;
+}
+
+	/*
+	** ################################################################
+	** METHOD opcode
+	** ################################################################
+	*/
+
+t_opcode Cpu::_getOpcode(uint8_t opcode)
 {
 	unsigned int i = 0;
-	s_opcode newOpcode;
 
-	bzero(newOpcode, sizeof(newOpcode));
-	while (register.getReg()[i])
+	while (i < NB_OPCODE)
 	{
-		if (register.getOpcode(register.getReg()[i]) == opcode)
-		{
-			newOpcode.execute = false;
-			newOpcode.opcode.opcode = opcode;
-			newOpcode.opcode.functionOpcode = register.getFunctionOpcode(register.getReg()[i]);
-			newOpcode.opcode.cycleOpcode = register.getCycleOpcode(register.getReg()[i]);
-			newOpcode.opcode.data = register.getDataOpcode(register.getReg()[i]);
-			newOpcode.opcode.lenghtData = register.getLenghtDataOpcode(register.getReg()[i]);
-			return (newreg);
+		if (this->opcodeMap[i].opcode == opcode)
+			return this->opcodeMap[i];
 		++i;
 	}
+	return this->opcodeMap[0];
 }
+
+uint8_t	Cpu::getCylceOpcode()
+{
+	if (this->register.F & mask > 0)
+		return this->cycleOpcodeFlag;
+	return cycleOpcodeNoFlag;
+}
+
 
 unsigned int Cpu::getCycleOpcode(void)
 {
