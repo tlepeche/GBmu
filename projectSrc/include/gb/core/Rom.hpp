cd453926aeb6e376bd605d0fd520d770773e77af
punkymaniac <droger@student.42.fr>
2016-11-10 09:37:47 +0100

    Delete variable inutile

--- projectSrc/include/gb/core/Rom.hpp
+++ projectSrc/include/gb/core/Rom.hpp
@@ -3,32 +3,32 @@
 
 #include "htype.hpp"
 
-typedef struct		info_rom
-{
-	htype			type;
-	char			cartridge;
-	char			romSize;
-	char			eramSize;
-}					irom;
+#define LOGO			0x0104
+#define TITLE			0x0134
+#define CGBFLAG			0x0143
+#define SGBFLAG			0x0146
+#define CARTRIDGE		0x0147
+#define ROMSIZE			0x0148
+#define RAMSIZE			0x0149
+#define HEADERCHECKSUM	0x014D
+#define GLOBALCHECKSUM	0x014D
 
 class Rom {
 
 	public:
 		static Rom		&Instance(void);
 		int				load(const char *file);
+		htype			getHardware(void);
 		bool			isLoaded(void);
 		uint8_t			read(uint16_t addr);
 		void			write(uint16_t addr, uint8_t val);
 		void			reset(void);
-		irom			getType(void);
 
 	private:
 		static Rom		_instance;
 		char			*_rom;
 		uint8_t			*_eram;
-		irom			_info;
-		uint8_t			_nbanks;
-		uint8_t			_nrambanks;
+		htype			_hardware;
 		uint8_t			_bank;
 		uint8_t			_rambank;
 		uint8_t			_tbank;
@@ -37,7 +37,6 @@ class Rom {
 		Rom(void);
 		~Rom(void);
 		void			init(void);
-		uint8_t			getBankRom(uint8_t octet);
 		uint8_t			getBankEram(uint8_t octet);
 };
 
