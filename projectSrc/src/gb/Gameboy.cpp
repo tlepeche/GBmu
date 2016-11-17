9026ecdc35e6f0f87226156b197b9370037cdfad
Thomas LEPECHEUR <tlepeche@e3r7p20.42.fr>
2016-11-17 17:16:44 +0100

    fix reset

--- projectSrc/src/gb/Gameboy.cpp
+++ projectSrc/src/gb/Gameboy.cpp
@@ -83,6 +83,7 @@ void	Gameboy::reset()
 	{
 		_willRun.store(true);
 		this->_memory->reset();
+		this->_clock->reset();
 		if (_memory->loadRom(_romPath.c_str(), this->_hardware))
 			_willRun.store(false);
 		else
