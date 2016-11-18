4707ad3169acb959f595168cb47bf014ff33477e
Michael barbarin <michael.barbarin@gmail.com>
2016-11-18 12:37:22 +0100

    divider add and mod DIV memory set

--- projectSrc/src/gb/core/Timer.cpp
+++ projectSrc/src/gb/core/Timer.cpp
@@ -11,6 +11,7 @@
 Timer::Timer(Memory *memory) :
 	_memory(memory),
 	_cycles(0),
+	_divider(0),
 	_cyclesAcc(0)
 {
 	this->_memory->write_byte(REGISTER_TIMA, 0x00);
@@ -27,19 +28,26 @@ Timer::Timer(Memory *memory) :
 
 void Timer::reset(void)
 {
+	this->_divider = 0;
 	this->_cycles = 0;
 }
 
-void	Timer::step(unsigned int cycles)
+void Timer::step(unsigned int cycles)
 {
 	uint8_t			tac = _memory->read_byte(REGISTER_TAC) & 0x3;
 	unsigned int	cyclesAccTima = Cpu_z80::getClockSpeed() / _arrayFrequency[tac];
 
 	_cyclesAcc += cycles;
+	_divider += cycles;
 	if (_cyclesAcc >= cyclesAccTima)
 	{
 		incTima();
 		_cyclesAcc -= cyclesAccTima;
+	}
+	if (_divider >= 0xFF)
+	{
+		incDivider();
+		_divider -= 0xFF;
 	}
 }
 
@@ -52,4 +60,14 @@ void	Timer::incTima(void)
 		_memory->write_byte(REGISTER_TIMA, tma);
 	else
 		_memory->write_byte(REGISTER_TIMA, tima + 1);
+}
+
+void	Timer::incDivider(void)
+{
+	uint8_t			div = _memory->read_byte(REGISTER_DIV);
+
+	if (div == 0xFF)
+		_memory->write_byte(REGISTER_DIV, 0x00);
+	else
+		_memory->write_byte(REGISTER_DIV, div + 1, true);
 }
